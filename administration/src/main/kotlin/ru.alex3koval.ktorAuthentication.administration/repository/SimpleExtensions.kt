617b3c1fa213dbfdd627f3ea126cb3af67695480
Alexandr Kovalenko <destructsymp@gmail.com>
2024-04-30 00:13:41 +0300

    Добавил расширения.

--- administration/src/main/kotlin/ru.alex3koval.ktorAuthentication.administration/repository/SimpleExtensions.kt
+++ administration/src/main/kotlin/ru.alex3koval.ktorAuthentication.administration/repository/SimpleExtensions.kt
@@ -2,6 +2,8 @@ package ru.alex3koval.ktorAuthentication.administration.repository
 
 import org.jetbrains.exposed.dao.id.EntityID
 import ru.alex3koval.ktorAuthentication.administration.vo.Activity
+import ru.alex3koval.ktorAuthentication.administration.vo.Email
+import ru.alex3koval.ktorAuthentication.administration.vo.Gender
 import kotlin.reflect.KClass
 import kotlin.reflect.full.primaryConstructor
 
@@ -11,5 +13,11 @@ import kotlin.reflect.full.primaryConstructor
 fun<T: EntityID<*>, R : Any> T.id(clazz: KClass<R>): R = clazz.primaryConstructor!!.call(value)
 
 /** Активность пользователя */
-inline val Boolean.activity: Activity
+val Boolean.activity: Activity
     get() = Activity(this)
+
+val Int.gender: Gender
+    get() = Gender(this).getOrThrow()
+
+val String.email: Email
+    get() = Email(this).getOrThrow()
