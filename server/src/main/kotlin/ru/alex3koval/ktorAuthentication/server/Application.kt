781abe2dd179a40301dadb956473ca73da3c4b08
Alexandr Kovalenko <destructsymp@gmail.com>
2024-04-30 18:43:52 +0300

    Расширение над ApplicationCall.

--- server/src/main/kotlin/ru/alex3koval/ktorAuthentication/server/Application.kt
+++ server/src/main/kotlin/ru/alex3koval/ktorAuthentication/server/Application.kt
@@ -1,6 +1,8 @@
 package ru.alex3koval.ktorAuthentication.server
 
 import io.ktor.server.application.*
+import org.koin.core.Koin
+import ru.alex3koval.ktorAuthentication.server.factory.DIBuilder
 import ru.alex3koval.ktorAuthentication.server.plugins.configureRouting
 import ru.alex3koval.ktorAuthentication.server.plugins.configureSecurity
 import ru.alex3koval.ktorAuthentication.server.plugins.configureSerialization
@@ -9,6 +11,9 @@ import ru.alex3koval.ktorAuthentication.server.plugins.configureSerialization
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
+
+val ApplicationCall.koin: Koin
+    get() = DIBuilder.di
 
 fun Application.module() {
     configureSecurity()
